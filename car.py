92d04f883a5c8fb707af3b36852a464590e2e804
andrey11obuhov <99744870+andrey11obuhov@users.noreply.github.com>
2022-11-16 09:29:53 +0300

    Add files via upload

---
+++ car.py
@@ -0,0 +1,24 @@
+class car(object):
+    def __init__(self, hp, wd, price, seats,
+    running, dtp, type, v_engine, 
+    engine_type, years, fuel_economy, acceleration, top_speed, 
+     v_trunk, box):
+        self.hp=hp
+        self.wd=wd
+        self.price=price
+        self.seats=seats
+        self.running=running
+        self.dtp=dtp
+        self.type=type
+        self.v_engine=v_engine
+        self.v_trunk=v_trunk
+        self.engine_type=engine_type
+        self.years=years
+        self.fuel_economy=fuel_economy
+        self.acceleration=acceleration
+        self.box=box
+
+
+
+
+
